e184f6eff42b2ac284c549da6f848b860899cddc
Shiyu Wang <shiyu3169@gmail.com>
2018-12-06 13:08:33 -0500

    build

---
+++ build/precache-manifest.f45ea39fa1c852a7d290b9be964a756e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bf9695ac3a5cb63d9de",
+    "url": "/static/js/main.5bf9695a.chunk.js"
+  },
+  {
+    "revision": "ca8f1190fdbecd1a6259",
+    "url": "/static/js/1.ca8f1190.chunk.js"
+  },
+  {
+    "revision": "5bf9695ac3a5cb63d9de",
+    "url": "/static/css/main.4ecd42c4.chunk.css"
+  },
+  {
+    "revision": "c1b391741e8eb17749baff5a58dfe1d9",
+    "url": "/index.html"
+  }
+];
