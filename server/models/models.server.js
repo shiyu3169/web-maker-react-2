3d136544faf77337c611ac31acbb1c03a85d1cb2
Shiyu Wang <shiyu3169@gmail.com>
2018-12-06 13:11:56 -0500

    mongoAuth

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,20 +1,20 @@
 var connectionString = "mongodb://127.0.0.1:27017/web-maker"; // for local
 
 if (process.env.MLAB_USERNAME_WEBDEV) {
-  // check if running remotely
+    // check if running remotely
 
-  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-  var password = process.env.MLAB_PASSWORD_WEBDEV;
-  connectionString = "mongodb://" + username + ":" + password;
-  connectionString += "@ds217864.mlab.com:17864/heroku_3r6bmlmd"; // use yours
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = "mongodb://" + username + ":" + password;
+    connectionString += "@ds121183.mlab.com:21183/heroku_cmtdczks"; // use yours
 }
 // import mongoose library
 var mongoose = require("mongoose");
 
 // connect server with database
 var db = mongoose.connect(
-  connectionString,
-  { useNewUrlParser: true }
+    connectionString,
+    { useNewUrlParser: true }
 );
 
 // export this file
